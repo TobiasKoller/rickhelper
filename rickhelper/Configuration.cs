40fbb91fa5053d1fe2b4c736d8fa99f33a787fc8
Tobias Koller <tobias.koller@outlook.com>
2022-02-09 07:33:11 +0100

    Add files via upload
    collection-creation

--- rickhelper/Configuration.cs
+++ rickhelper/Configuration.cs
@@ -16,15 +16,16 @@ namespace rickhelper
     {
         [JsonProperty("originals")]
         public List<string> Originals { get; set; }
+
         [JsonProperty("replace_with")]
         public string ReplaceWith { get; set; }
+
+        [JsonProperty("cfg_file_name")]
+        public string CfgFileName { get; set; }
     }
 
     public class CollectionFixerConfig : IConfig
     {
-
-        [JsonProperty("collection_dir")]
-        public string CollectionDirectory { get; set; }
 
         [JsonProperty("collection_out_dir")]
         public string CollectionOutDirectory { get; set; }
@@ -35,7 +36,6 @@ namespace rickhelper
         {
             var color = ConsoleColor.Yellow;
             Cmd.Write("Options:");
-            Cmd.Write("collection_dir=" + CollectionDirectory, color);
             Cmd.Write("collection_out_dir=" + CollectionOutDirectory, color);
             Cmd.Spacer();
         }
@@ -45,14 +45,22 @@ namespace rickhelper
     {
         [JsonProperty("gamelist_dir")]
         public string GamelistXmlDirectory { get; set; }
+
         [JsonProperty("gamelist_out_dir")]
         public string GamelistXmlOutDirectory { get; set; }
-        [JsonProperty("delete_cfg_file")]
+
+        [JsonProperty("image_extension")]
         public string ImageExtension { get; set; }
+
         [JsonProperty("max_description_length")]
         public int MaxDescriptionLength { get; set; }
+
+        [JsonProperty("ignore_games")]
+        public List<string> IgnoreGames { get; set; }
+
         [JsonProperty("verbose")]
         public bool Verbose { get; set; }
+
         public void ToConsole()
         {
             var color = ConsoleColor.Yellow;
@@ -60,6 +68,7 @@ namespace rickhelper
             Cmd.Write("gamelist_dir=" + GamelistXmlDirectory, color);
             Cmd.Write("gamelist_out_dir=" + GamelistXmlOutDirectory, color);
             Cmd.Write("max_description_length=" + MaxDescriptionLength, color);
+            Cmd.Write("ignore_games=" + string.Join(",", IgnoreGames), color);
             Cmd.Write("image_extension=" + ImageExtension, color);
             Cmd.Write("verbose=" + Verbose, color);
             Cmd.Spacer();
